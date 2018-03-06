6affb43a3d58383d8912188602a53191b64fa1ad
Shachar Oz <shachar.oz@gmail.com>
2018-03-06 20:50:04 +0200

    animation sample

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    private Animator _animator;
+
+    public string ActivateLandingTriggerName;
+
+    // Use this for initialization
+    void Start () {
+        _animator = GetComponent<Animator>();
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger(ActivateLandingTriggerName);
+        }
+        
+	}
+}
