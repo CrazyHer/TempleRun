dfcaf43be4d8838acf097a80d09500385286ffba
LiamLin <605761792@qq.com>
2021-11-23 20:21:17 +0800

    implement rotate 90 degree

--- Assets/Player/Movement.cs
+++ Assets/Player/Movement.cs
@@ -75,7 +75,8 @@ public class Movement : MonoBehaviour
         {
             Debug.Log("接收到socket消息：" + server.GetMessageBuffer()+"Count:"+server.count);
         }
-        if(this.GetComponent<Transform>().position.y<=0){
+        if(this.GetComponent<Transform>().position.y<=-0.1){
+            //Debug.Log(this.GetComponent<Transform>().position.y);
             CallGameOver();
         }
 
@@ -122,12 +123,22 @@ public class Movement : MonoBehaviour
         //    transform.Translate(Vector3.up* Time.deltaTime * _speed * 5);
         this.GetComponent<Rigidbody>().velocity = Vector3.up * 15;
        }
-       /*
-       if (Input.GetKey(KeyCode.DownArrow))
-       {
-           transform.Translate(Vector3.back * Time.deltaTime * _speed);
-       }
-       */
+
+        if (useSmoothRide)
+        {
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                //transform.Translate(Vector3.back * Time.deltaTime * _speed);
+                this.transform.Rotate(Vector3.up * Time.deltaTime * _speed);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                this.transform.Rotate(Vector3.up * 90);
+            }
+        }
     }
 
 
